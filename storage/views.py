ee87500191c5ef7e3caf498c681dbbd6b69c3b3f
aman248 <baman248@gmail.com>
2020-12-30 19:55:56 +0530

    second commit

--- storage/views.py
+++ storage/views.py
@@ -8,29 +8,15 @@ from django.contrib.auth.models import User
 # Create your views here.
 @login_required
 def index(request):
-    print(request.user)
-    tasks = Task.objects.all().filter(assigned_to=request.user,is_accepted=True)
     profile = Profile.objects.all().filter(user=request.user)
-    alerts = Task.objects.all().filter(is_accepted=False)
-    submits = Task.objects.all().filter(assigned_by=request.user,is_submited=True,done=False)
-    return render(request,'storage/index.html',{'tasks':tasks,'profile':profile,'alerts':alerts,'submits':submits})
-
-@login_required
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        print('At upper side')
-        print(form.is_valid())
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request,f'account created for {username}')
-            print('Hi man a am here')
-            return redirect(reverse('login'))
+    if profile:
+        print(request.user)
+        tasks = Task.objects.all().filter(assigned_to=request.user,is_accepted=True)
+        alerts = Task.objects.all().filter(is_accepted=False,assigned_to=request.user)
+        submits = Task.objects.all().filter(assigned_by=request.user,is_submited=True,done=False)
+        return render(request,'storage/index.html',{'tasks':tasks,'profile':profile,'alerts':alerts,'submits':submits})
     else:
-        form = UserRegisterForm()
-        print('At outside')
-    return render(request,'storage/register.html',{'form':form})
+        return complete_profile(request)
 def complete_profile(request):
     if request.method == 'POST':
         form = ProfileCreationForm(request.POST)
@@ -42,8 +28,7 @@ def complete_profile(request):
             phone_no = form.cleaned_data.get('phone_no')
             user = request.user
             profile = Profile(skills=skills,email=email,phone_no=phone_no,user=user)
-            
-            #profile.save()
+            profile.save()
             return redirect(reverse('storage:index'))
     else:
         print('this is an empty form')
